3a62ca906f29ed06491ea556a99de03e591c37e4
Neozuni <30896375+Neozuni@users.noreply.github.com>
2017-10-22 18:16:42 +0900

    #0003: bugFIx
    UpdateFormAction.java
    #0003: errorFix +String pageNum

--- self_Board/src/board/action/UpdateFormAction.java
+++ self_Board/src/board/action/UpdateFormAction.java
@@ -9,8 +9,12 @@ import board.model.BoardVO;
 public class UpdateFormAction implements CommandAction {
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		int num = Integer.parseInt(request.getParameter("pageNum"));
+		// #0003: errorFix +String pageNum
+		String pageNum = request.getParameter("pageNum");
+		
 		BoardDAO dbPro = BoardDAO.getInstance();
 		BoardVO article = dbPro.updateGetArticle(num);
+		
 		//해당 뷰에서 사용할 속성
 		request.setAttribute("pageNum", new Integer(pageNum));
 		request.setAttribute("article", article);
